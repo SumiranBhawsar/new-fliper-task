267835c31c225440ec36310af1d4ab62714198d1
SumiranBhawsar <sumiranbhawsar149@gmail.com>
2025-07-11 17:08:31 +0530

    new branch

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,6 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
 
 app.use(cookieParser());
 
--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,26 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import seedDefaultAdmin from "./seed/seedDefaultAdmin.js";
+import path from "path";
+import express from "express";
 
 dotenv.config({
   path: "./env",
 });
 
+const __dirname = path.resolve();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// ✅ Correct wildcard route
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 connectDB()
   .then(async () => {
-    // 👇 seed admin after DB connection
     await seedDefaultAdmin();
-
     app.listen(process.env.PORT || 8000, () => {
       console.log(`🚀 Server running on port ${process.env.PORT}`);
     });
